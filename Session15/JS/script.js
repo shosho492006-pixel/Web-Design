7d75e221085700ddc07a532ecab87fd837eae6b5
Shahd Algohary <shosho492006@gmail.com>
2026-09-05 20:13:00 +0300

    Edit Session15

--- Session15/JS/script.js
+++ Session15/JS/script.js
@@ -8,3 +8,21 @@ let handleMouseEvent = (element) => {
 
     element.classList.add("bg-primary")
 }
+
+let users = []
+
+function addUser(){
+
+    let name = document.getElementById("name").value
+
+    let email = document.getElementById("email").value
+
+    let user = {
+        name: name,
+        email: email
+    }
+
+    users.push(user)
+
+    console.log(users)
+}
